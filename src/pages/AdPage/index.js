4c397a4000a900b2ee0eade375b8c55fd5494491
Luan Farias <matheus.frs22123@outlook.com>
2020-05-26 20:20:35 -0300

    AdPage/Ad was maden

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+import { Slide } from 'react-slideshow-image';
 
 import { PageArea, Fake } from './styles';
 import { PageContainer } from '../../components/MainComponents';
@@ -12,22 +13,64 @@ export default function SignIn() {
     const [loading, setLoading] = useState(true);
     const [adInfo, setAdInfo] = useState([]);
 
+    useEffect(()=> {
+        const getAdInfo = async (id) => {
+            const json = await api.getAd(id, true);
+
+            setAdInfo(json);
+            setLoading(false);
+        }
+
+        getAdInfo(id);
+    }, [api, id]);
+
+    const formatDate = (date) => {
+        let cDate = new Date (date);
+
+        let months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+        let cDay = cDate.getDate();
+        let cMonth = cDate.getMonth();
+        let cYear = cDate.getFullYear();
+
+        return `${cDay} de ${months[cMonth]} de ${cYear}`;
+    }
+
     return (
         <PageContainer>
             <PageArea>
                 <div className="leftSide">
                     <div className="box">
                         <div className="adImage">
-                         {loading && <Fake height={300} />}
+                            {loading && <Fake height={300} />}
+                            {adInfo.images &&
+                                <Slide>
+                                    {adInfo.images.map((image, k) => (
+                                        <div key={k} className="eachSlide">
+                                            <img src={image} alt=""/>
+                                        </div>
+                                    ))}
+                                </Slide>
+                            }
                         </div>
 
                         <div className="adInfo">
                             <div className="adName">
                                 {loading && <Fake hwight={20} />}
+                                {adInfo.title && 
+                                    <h2>{adInfo.title}</h2>
+                                }
+                                {adInfo.dateCreated && 
+                                    <small>Criado em {formatDate(adInfo.dateCreated)} </small>
+                                }
                             </div>
 
                             <div className="adDescription">
                                 {loading && <Fake height={100} />}
+                                {adInfo.description}
+                                <hr />
+                                {adInfo.views &&
+                                    <small>Visualizações: {adInfo.views}</small>
+                                } 
                             </div>
                         </div>
                     </div>
@@ -36,11 +79,25 @@ export default function SignIn() {
                 <div className="rightSide">
                     <div className="box boxPadding">
                         {loading && <Fake height={20} />}
+                        {adInfo.priceNegotiable &&
+                            'Preço Negociável'
+                        }
+                        {!adInfo.priceNegotiable && adInfo.price &&
+                            <div className="price">Preço: <span>R$ {adInfo.price}</span></div>
+                        }
                     </div>
+                    {loading && <Fake height={50} />}
+                    {adInfo.userInfo &&
+                        <>
+                            <a href={`mailto:${adInfo.userInfo.email}`} target="_blank" className="contactSellerLink">Fale com o vendedor</a>
 
-                    <div className="box boxPadding">
-                        {loading && <Fake height={50} />}
-                    </div>
+                            <div className="createdBy box boxPadding">
+                                <strong>{adInfo.userInfo.name}</strong>
+                                <small>Email: {adInfo.userInfo.email}</small>
+                                <small>Estado: {adInfo.stateName}</small>
+                            </div>
+                        </>
+                    }
                 </div>
             </PageArea>
         </PageContainer>
--- src/pages/AdPage/styles.js
+++ src/pages/AdPage/styles.js
@@ -26,24 +26,83 @@ export const PageArea = styled.div`
         flex: 1;
         margin-right: 20px;
 
-        .adImage{
+        .box {
+            display: flex;
+        }
 
+        .adImage{
+            width: 320px;
+            height: 320px;
+            margin-right: 20px;
+
+            .eachSlide img {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-size: cover;
+                height: 320px;
+            }
         }
 
         .adInfo {
-            padding: 10px;
+            flex: 1;
 
             .adName {
                 margin-bottom: 20px;
+
+                h2 {
+                    margin: 0;
+                    margin-top: 20px;
+                }
+
+                small {
+                    color: #999;
+                }
             }
 
             .adDescription {
 
+                small {
+                    color: #999;
+                }
             }
         }
     }
 
     .rightSide {
         width: 250px;
+
+        .price span {
+            color: #00F;
+            display: block;
+            font-size: 27px;
+            font-weight: bold;
+        }
+
+        .contactSellerLink {
+            background-color: #007bff;
+            color: #FFF;
+            height: 30px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 4px #999;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-decoration: none;
+            margin-bottom: 20px;
+        }
+
+        .createdBy {
+
+            strong {
+                display: block;
+            }
+
+            small {
+                display: block;
+                color: #999;
+                margin-top: 10px;
+            }
+        }
     }
 `;
